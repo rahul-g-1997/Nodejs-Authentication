429b6cbbecb61349427dcd1f2b00a3aaf43e08e6
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-12-27 15:44:58 +0530

    refactor: clean up comments in passport.js initialization function

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -2,12 +2,9 @@
 import { Strategy as LocalStrategy } from "passport-local";
 
 // Import the User model for accessing the authentication methods provided by the model
-import User from "../models/user.js"; // Ensure the correct extension (.js) is included for local imports
+import User from "../models/user.js"; 
 
-/*
- * Function to initialize Passport.js with the local authentication strategy
- * and serialize/deserialize user methods
- */
+//Function to initialize Passport.js with the local authentication strategy
 function init(passport) {
   // Configure Passport to use the LocalStrategy with methods provided by the User model
   passport.use(new LocalStrategy(User.authenticate()));
