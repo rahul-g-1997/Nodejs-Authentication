dc8d8953ab2f7e5ede3a614938a124ca795168e8
Rahul Gaydhane <rahulgaydhane7@gmail.com>
2024-12-27 15:47:44 +0530

    refactor: remove redundant comments in user model schema

--- src/models/user.js
+++ src/models/user.js
@@ -10,25 +10,20 @@ const { Schema } = mongoose;
 // Define the user schema to structure user data in the database
 const userSchema = new Schema(
   {
-    // Username field: required and stored as a string
     username: { type: String, required: true },
 
-    // Email field: required, unique, and stored as a string
     email: { type: String, required: true, unique: true },
 
-    // Password field: optional and stored as a string (hashing will be handled by passport-local-mongoose)
     password: { type: String },
   },
-  // Enable automatic timestamps for creation and update times
+
   { timestamps: true }
 );
 
-// Add passport-local-mongoose plugin to the schema
 // This plugin simplifies password management by adding methods for hashing passwords, authentication, and more
 userSchema.plugin(passportLocalMongoose);
 
 // Create a model using the schema
-// This provides an interface to interact with the 'users' collection in the database
 const User = mongoose.model("User", userSchema);
 
 // Export the model to use it in other parts of the application
